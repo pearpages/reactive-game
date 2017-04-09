1ee36d5f78ceb6fae00b920dbe44dedbf3d411ea
pearpages <pere@soms.cat>
2017-04-09 23:10:30 +0200

    render space

--- src/spaceship.js
+++ src/spaceship.js
@@ -3,17 +3,20 @@
 (function () {
     var SPEED = 10;
     var STAR_NUMBER = 250;
+    var SPACE_COLOR = '#000000';
+    var STARS_COLOR = '#ffffff';
+    var MAX_STAR_SIZE = 3;
 
-    init();
+    init(SPEED,STAR_NUMBER,SPACE_COLOR,STARS_COLOR,MAX_STAR_SIZE);
 
-    function init() {
+    function init(SPEED, STAR_NUMBER, SPACE_COLOR, STARS_COLOR,MAX_STAR_SIZE) {
         var canvas = document.createElement('canvas');
         var ctx = canvas.getContext("2d");
-        attachCanvas(canvas,ctx);
-        var StarStream = getStarStream(canvas, ctx, SPEED, STAR_NUMBER)
-        .subscribe(function (starArray) {
-            paintStars(starArray, canvas, ctx);
-        });
+        attachCanvas(canvas, ctx);
+        getStarStream(canvas, ctx, SPEED, STAR_NUMBER,MAX_STAR_SIZE)
+            .subscribe(function (starArray) {
+                paintStars(starArray, canvas, ctx, SPACE_COLOR, STARS_COLOR);
+            });
     }
 
     function attachCanvas(canvas, ctx) {
@@ -22,13 +25,13 @@
         canvas.height = window.innerHeight;
     }
 
-    function getStarStream(canvas, ctx, speed, starNumber) {
+    function getStarStream(canvas, ctx, SPEED, STAR_NUMBER,MAX_STAR_SIZE) {
         return Rx.Observable.range(1, STAR_NUMBER)
             .map(function () {
                 return {
                     x: parseInt(Math.random() * canvas.width),
                     y: parseInt(Math.random() * canvas.height),
-                    size: Math.random() * 1 + 1
+                    size: Math.random() * MAX_STAR_SIZE + 1
                 };
             })
             .toArray()
@@ -46,10 +49,10 @@
             })
     }
 
-    function paintStars(stars, canvas, ctx) {
-        ctx.fillStyle = '#000000';
+    function paintStars(stars, canvas, ctx, spaceColor, starsColor) {
+        ctx.fillStyle = spaceColor;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = '#ffffff';
+        ctx.fillStyle = starsColor;
         stars.forEach(function (star) {
             ctx.fillRect(star.x, star.y, star.size, star.size);
         });
